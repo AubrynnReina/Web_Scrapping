fa103729024fa7ce8af5b78249b07832093e7a56
AubReina <anh007008009@gmail.com>
2024-09-19 15:50:18 +0700

    remove append data function and add latest ep field

--- modules/processor.py
+++ modules/processor.py
@@ -1,7 +1,8 @@
-def convert_to_dict(id, data) -> dict:
+def convert_to_dict(id, latest_ep, data) -> dict:
 
     result_dict = {
         'id': id,
+        'latest_ep': latest_ep,
         'name': data[0],
         'genre': data[1],
         'status': data[2],
@@ -12,21 +13,3 @@ def convert_to_dict(id, data) -> dict:
         'link': data[7]
     }
     return result_dict
-
-
-def append_data(id, data, data_dict):
-    
-    for i in range(len(data[1])):
-
-        for j, key in enumerate(data_dict):
-
-            if j == 0:  
-                data_dict[key].append(id)
-            
-            elif j == 2:
-                data_dict[key].append(data[j - 1][i])
-                
-            else:
-                data_dict[key].append(data[j - 1])
-
-    return data_dict
